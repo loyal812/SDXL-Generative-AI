f5788b976dbecef40e8c46af58ca8d873630b6ba
eureka320 <elias.h0320@gmail.com>
2024-03-30 08:04:19 -0400

    refactor: txt2img feature
    Co-authored-by: loyal812 <jh.chan0812@gmail.com>

--- api/scripts/txt2img.py
+++ api/scripts/txt2img.py
@@ -20,6 +20,27 @@ def txt2img(param: Txt2ImgRequest):
         model = load_sdxl_refiner_model(MODEL_TYPE, MODEL_LOAD_TYPE)
 
     # txt2img
-    image = model(param.prompt).images[0]
+    image = model(
+        prompt = param.prompt,
+        prompt2 = param.prompt2,
+        height = param.height,
+        width = param.width,
+        num_inference_steps = param.num_inference_steps,
+        denoising_end = param.denoising_end,
+        guidance_scale = param.guidance_scale,
+        negative_prompt = param.negative_prompt,
+        negative_prompt_2 = param.negative_prompt_2,
+        num_images_per_prompt = param.num_images_per_prompt,
+        eta = param.eta,
+        output_type = param.output_type,
+        return_dict = param.return_dict,
+        guidance_rescale = param.guidance_rescale,
+        original_size = param.original_size,
+        crops_coords_top_left = param.crops_coords_top_left,
+        target_size = param.target_size,
+        negative_original_size = param.negative_original_size,
+        negative_crops_coords_top_left = param.negative_crops_coords_top_left,
+        negative_target_size = param.negative_target_size
+    ).images[0]
 
     return image
